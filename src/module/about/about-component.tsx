4bb9e3f4af395c1d18ec8d4343716ba1031bbc03
HusniddinAyubjonov <husniddinayubjonov@gmail.com>
2025-12-06 16:14:57 +0500

    added ourMission

--- src/module/about/about-component.tsx
+++ src/module/about/about-component.tsx
@@ -1,9 +1,10 @@
-import OurMission from "./components/OurMission/OurMission";
-import OurSkils from "./components/OurSkils/OurSkils";
+// import OurSkils from "./components/OurSkils/OurSkils";
 import styles from "./about.module.css";
-import { PrincipleList } from "./principle-list";
+import { missionData, PrincipleList } from "./constants";
 import { Navbar } from "../../ui-components/ui/Navbar/Navbar";
 import { Footer } from "../../ui-components/ui/Footer/Footer";
+
+import img from "../../assets/imgs/planet.png";
 
 export default function About() {
   // ---------------------------------------------------------------------------
@@ -25,32 +26,66 @@ export default function About() {
       /// Our Principles
       --------------------------------------------------------------------------- */}
 
-      <div className={styles.ourPrinciples}>
-        <span className={styles.ourPrincipleTexts}>
-          <h1 className={styles.ourPrincipleTitle}>Our Principles</h1>
-          <p className={styles.ourPrincipleDescription}>
-            Our principles are quality, transparency, responsibility, and a
-            personalized approach to every project.
-          </p>
-        </span>
+      <section>
+        <div className={styles.ourPrinciples}>
+          <span className={styles.ourPrincipleTexts}>
+            <h1 className={styles.ourPrincipleTitle}>Our Principles</h1>
+            <p className={styles.ourPrincipleDescription}>
+              Our principles are quality, transparency, responsibility, and a
+              personalized approach to every project.
+            </p>
+          </span>
 
-        <div className={styles.principleCards}>
-          {PrincipleList.map((principle, index) => (
-            <div className={styles.principleCard} key={index}>
-              <div className={styles.principleCardIcons}>
-                <img className={styles.principleCardIcon} src={principle.icon} alt="icon" />
+          <div className={styles.principleCards}>
+            {PrincipleList.map((principle, index) => (
+              <div className={styles.principleCard} key={index}>
+                <div className={styles.principleCardIcons}>
+                  <img
+                    className={styles.principleCardIcon}
+                    src={principle.icon}
+                    alt="icon"
+                  />
+                </div>
+                <div className={styles.principleCardText}>
+                  <h2 className={styles.principleCardTitle}>
+                    {principle.title}
+                  </h2>
+                  <p className={styles.principleCardDescription}>
+                    {principle.description}
+                  </p>
+                </div>
               </div>
-              <div className={styles.principleCardText}>
-                <h2 className={styles.principleCardTitle}>{principle.title}</h2>
-                <p className={styles.principleCardDescription}>{principle.description}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
+      </section>
 
-      <OurMission />
-      <OurSkils />
+      {/*---------------------------------------------------------------------------
+      /// Our Mission
+      --------------------------------------------------------------------------- */}
+
+      <section>
+        <section className={styles.section}>
+          <div className={styles.ourMission}>
+            <div className={styles.infoBox}>
+              {missionData.map((texts, index) => (
+                <div key={index} className={styles.infoTexts}>
+                  <h2 className={styles.infoTitle}>{texts.title}</h2>
+                  <span className={styles.descriptions}>
+                    <p className={styles.description}>{texts.description}</p>
+                    <p className={styles.secondDescription}>
+                      {texts.descriptionSecond}
+                    </p>
+                  </span>
+                </div>
+              ))}
+            </div>
+          </div>
+          <img className={styles.worldImg} src={img} alt="world img" />
+        </section>
+      </section>
+
+      {/* <OurSkils /> */}
       <Footer />
     </div>
   );
--- src/module/about/constants.ts
+++ src/module/about/constants.ts
@@ -1,4 +1,36 @@
-export const Mission = [
+import icon from "../../assets/icon/principles-icon1.svg";
+import icon2 from "../../assets/icon/principles-icon2.svg";
+import icon3 from "../../assets/icon/principles-icon3.svg";
+import icon4 from "../../assets/icon/principles-icon4.svg";
+
+export const PrincipleList = [
+  {
+    icon: icon,
+    title: "Ethics and Responsibility",
+    description:
+      "We build safe, ethical products, avoiding illegal or adult content, gambling, and trivial projects, focusing on meaningful experiences for users.",
+  },
+  {
+    icon: icon2,
+    title: "Contract and Respec",
+    description:
+      "We sign formal contracts before projects and value mutual respect and trust; we reserve the right to end partnerships if our team is disrespected.",
+  },
+  {
+    icon: icon3,
+    title: "We Hold Client Calls",
+    description:
+      "We hold calls with clients to clarify project details, align expectations, and ensure desired results, allowing us to respond quickly to changes and build trust.",
+  },
+  {
+    icon: icon4,
+    title: "Transparency and accountability",
+    description:
+      "We keep clients informed throughout the project and provide reports at each stage.",
+  },
+];
+
+export const missionData = [
     {
         title: 'Our mission',
         description:
